b04a331845308d30209b99b0e327ecd658cdf160
Martin Schubert <mfschubert@gmail.com>
2024-10-09 13:00:31 -0700

    duplicate failing test

--- tests/optimizers/test_debug.py
+++ tests/optimizers/test_debug.py
@@ -68,6 +68,55 @@ class VmapTest(unittest.TestCase):
             batch_values, onp.transpose(no_batch_values, (1, 0)), atol=1e-4
         )
 
+    def test_optimization_with_vmap_2(self):
+        def initial_params_fn(key):
+            ka, kb = jax.random.split(key)
+            return {
+                "a": jax.random.normal(ka, (10,)),
+                "b": jax.random.normal(kb, (10,)),
+                "c": types.Density2DArray(array=jnp.ones((3, 3))),
+            }
+
+        def loss_fn(params):
+            flat, _ = flatten_util.ravel_pytree(params)
+            return jnp.sum(jnp.abs(flat**2))
+
+        keys = jax.random.split(jax.random.PRNGKey(0))
+        opt = lbfgsb.density_lbfgsb(beta=2, maxcor=20)
+
+        # Test batch optimization
+        params = jax.vmap(initial_params_fn)(keys)
+        state = jax.vmap(opt.init)(params)
+
+        @jax.jit
+        @jax.vmap
+        def step_fn(state):
+            params = opt.params(state)
+            value, grad = jax.value_and_grad(loss_fn)(params)
+            state = opt.update(grad=grad, value=value, params=params, state=state)
+            return state, value
+
+        batch_values = []
+        for i in range(10):
+            state, value = step_fn(state)
+            batch_values.append(value)
+
+        # Test one-at-a-time optimization.
+        no_batch_values = []
+        for k in keys:
+            no_batch_values.append([])
+            params = initial_params_fn(k)
+            state = opt.init(params)
+            for _ in range(10):
+                params = opt.params(state)
+                value, grad = jax.jit(jax.value_and_grad(loss_fn))(params)
+                state = opt.update(grad=grad, value=value, params=params, state=state)
+                no_batch_values[-1].append(value)
+
+        onp.testing.assert_allclose(
+            batch_values, onp.transpose(no_batch_values, (1, 0)), atol=1e-4
+        )
+
     def test_optimization_with_vmap_only(self):
         def initial_params_fn(key):
             ka, kb = jax.random.split(key)
