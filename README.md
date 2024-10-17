ed2d45b271acb2fac26143fccb8a3be8b832c219
Martin Schubert <martinschubert@Martins-MBP.attlocal.net>
2024-10-17 09:58:50 -0700

    Version updated from v0.9.3 to v0.9.4

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # invrs-opt - Optimization algorithms for inverse design
-`v0.9.3`
+`v0.9.4`
 
 ## Overview
 
--- src/invrs_opt/__init__.py
+++ src/invrs_opt/__init__.py
@@ -3,7 +3,7 @@
 Copyright (c) 2023 The INVRS-IO authors.
 """
 
-__version__ = "v0.9.3"
+__version__ = "v0.9.4"
 __author__ = "Martin F. Schubert <mfschubert@gmail.com>"
 
 from invrs_opt import parameterization as parameterization
