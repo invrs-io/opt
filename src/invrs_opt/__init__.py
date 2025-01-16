d0919ba293b3c9f16ad7368e2db6a06ab3b1a99d
Martin Schubert <martinschubert@Mac.attlocal.net>
2025-01-16 11:04:58 -0800

    Version updated from v0.10.4 to v0.10.5

--- src/invrs_opt/__init__.py
+++ src/invrs_opt/__init__.py
@@ -3,7 +3,7 @@
 Copyright (c) 2023 The INVRS-IO authors.
 """
 
-__version__ = "v0.10.4"
+__version__ = "v0.10.5"
 __author__ = "Martin F. Schubert <mfschubert@gmail.com>"
 
 from invrs_opt import parameterization as parameterization
